208413d4734f9d55ab6f04a82cec35b67a397243
Roman Mahotsky <Roman_Mahotsky@epam.com>
2019-08-28 15:11:05 +0300

    Update port

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer  = require('json-server')
 const server      = jsonServer.create()
 const router      = jsonServer.router(require('./routes.js')())
 const middlewares = jsonServer.defaults()
+const port = 1337;
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, function () {
-  console.log('JSON Server is running on port 3000!')
+server.listen(port, function () {
+  console.log(`Server is running on port ${port}!`)
 })
